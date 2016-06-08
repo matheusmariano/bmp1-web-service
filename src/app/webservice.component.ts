b1ba55b05651217c023c4b9e45171ed04f60d563
Matheus Mariano <vmatheus.mariano@gmail.com>
2016-06-08 13:24:16 -0300

    Improve layout

--- src/app/webservice.component.ts
+++ src/app/webservice.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IndexComponent } from './+index';
 import { RouteConfig , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router-deprecated';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card'
+import { IndexComponent } from './+index';
 import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
 import { NewComponent } from './+new';
 
@@ -9,7 +10,7 @@ import { NewComponent } from './+new';
   selector: 'webservice-app',
   templateUrl: 'webservice.component.html',
   styleUrls: ['webservice.component.css'],
-  directives: [ROUTER_DIRECTIVES, MD_TOOLBAR_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, MD_CARD_DIRECTIVES, MD_TOOLBAR_DIRECTIVES],
   providers: [ROUTER_PROVIDERS]
 })
 @RouteConfig([
